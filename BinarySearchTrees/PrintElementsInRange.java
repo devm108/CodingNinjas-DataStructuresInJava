f5e60a72df880f7719492a7e640f92fcf697812d
Dev Mittal <53218476+devm108@users.noreply.github.com>
2019-10-24 12:50:18 +0530

    Create PrintElementsInRange.java

---
+++ BinarySearchTrees/PrintElementsInRange.java
@@ -0,0 +1,32 @@
+public class Solution {
+
+/*	Binary Tree Node class 
+ * 
+ * 	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+		
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
+	*/
+	
+	public static void printNodeFromK1ToK2(BinaryTreeNode<Integer> root,int k1,int k2){
+		if (root == null) {
+			return;
+		}
+		if (k1 < root.data) {
+			printNodeFromK1ToK2(root.left, k1, k2);
+		}
+		if (k1 <= root.data && k2 >= root.data) { 
+            System.out.print(root.data + " "); 
+        }
+		if (k2 > root.data) { 
+            printNodeFromK1ToK2(root.right, k1, k2); 
+        } 
+	}
+	
+	
+}
